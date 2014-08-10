22d7e7d3a14c8e385f6df04659ef562648e09af5
Sid Tantia <sstantia@gmail.com>
2014-08-10 19:33:43 -0400

    ember automatically does this so don't need this code

--- app/assets/javascripts/routes/type_route.js
+++ app/assets/javascripts/routes/type_route.js
@@ -1,7 +1,5 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 Basebox.TypeRoute = Ember.Route.extend({
-	model: function(params) {
-		return this.store.find('type', params.type_id);
-	}
+	
 });
