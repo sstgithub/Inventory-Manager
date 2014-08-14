4e9ac4d14791d7bf07057e1c203042195b388c28
Sid Tantia <sstantia@gmail.com>
2014-08-14 07:49:09 -0700

    Changed so it validates uniqueness of metal name and type name

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,6 +1,7 @@
 class Type < ActiveRecord::Base
-	validates :type_name, :presence => true, :uniqueness => true
+	validates :type_name, :presence => true
 	validates :metal_name, :presence => true
+	validates_uniqueness_of :metal_name, scope: :type_name
 	validates :quantity, :presence => true
 
 	has_many :attribs
